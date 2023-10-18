24db131d2127e48735604cb1d298efdb23dc7cf1
lopo <lopo@zju.edu.cn>
2023-10-18 17:01:22 +0800

    style: restructure

---
+++ .vitepress/theme/layouts/Outline.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import TreeBranch from "../components/TreeBranch.vue";
+
+const { tree } = defineProps<{
+    tree: Record<string, string[]>
+}>()
+</script>
+
+<template>
+    <div class="tree-outline">
+        <h2 style="text-align: center; font-size: 20px;">Outline</h2>
+        <TreeBranch v-for="(v, k) in tree" :label="k" :items="v"/>
+    </div>
+</template>
+
+<style scoped>
+.tree-outline {
+    width: 100%;
+    height: 100%;
+    padding: 32px;
+    overflow: auto;
+    display: flex;
+    flex-direction: column;
+}
+</style>
