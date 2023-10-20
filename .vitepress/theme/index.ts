9eb53db8dd59fbb79c6141dc7aadd6dfc2c5423a
lopo <lopo@zju.edu.cn>
2023-10-20 18:04:13 +0800

    feat(theme): scroll-bar style

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,12 @@
+import './index.css'
+
 import DefaultTheme from 'vitepress/theme'
 // @ts-ignore
 import Outline from './layouts/Outline.vue'
 
 export default {
     extends: DefaultTheme,
-    enhanceApp({app}) {
+    enhanceApp({ app }) {
         app.component('outline', Outline)
     }
 }
