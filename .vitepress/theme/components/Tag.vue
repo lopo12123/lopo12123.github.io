0f01a814247cafc1e26ee7ea29dd645c437619fa
lopo <lopo@zju.edu.cn>
2023-10-21 00:28:19 +0800

    feat(theme): tag component

---
+++ .vitepress/theme/components/Tag.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+const {type} = defineProps<{
+  text: string
+  type: 'PLATFORM' | 'LANGUAGE' | 'TAG'
+}>()
+</script>
+
+<template>
+  <span :class="['base', type]">{{ type }}: {{ text }}</span>
+</template>
+
+<style scoped>
+.base {
+  height: 24px;
+  margin-right: 4px;
+  padding: 0 10px;
+  border: 1px solid transparent;
+  border-radius: 12px;
+  line-height: 22px;
+  font-size: 12px;
+  font-weight: 500;
+  cursor: pointer;
+  flex-shrink: 0;
+  display: inline-block;
+}
+
+.PLATFORM {
+  border-color: var(--vp-badge-danger-border);
+  color: var(--vp-badge-danger-text);
+  background-color: var(--vp-badge-danger-bg);
+}
+
+.LANGUAGE {
+  border-color: var(--vp-badge-warning-border);
+  color: var(--vp-badge-warning-text);
+  background-color: var(--vp-badge-warning-bg);
+}
+
+.TAG {
+  border-color: var(--vp-badge-tip-border);
+  color: var(--vp-badge-tip-text);
+  background-color: var(--vp-badge-tip-bg);
+}
+</style>
