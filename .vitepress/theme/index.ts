e36b2c1bd2bf014f549b5e8eed0515d0434810eb
lopo <lopo@zju.edu.cn>
2023-10-22 00:13:55 +0800

    feat(theme): custom layout

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,10 +4,14 @@ import type {Theme} from "vitepress";
 
 import DefaultTheme from 'vitepress/theme'
 import CustomLayout from "./layouts/CustomLayout.vue";
+import Outline from "./layouts/Outline.vue";
 
 const theme: Theme = {
-    // extends: DefaultTheme,
-    ...DefaultTheme,
+    extends: DefaultTheme,
+    // ...DefaultTheme,
+    enhanceApp({app}) {
+        app.component('Outline', Outline)
+    },
     Layout: CustomLayout,
 }
 
