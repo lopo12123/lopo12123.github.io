bbf7364474d524438d6732dafb84a9a927c68ceb
lopo <lopo@zju.edu.cn>
2023-10-22 15:48:58 +0800

    feat: scanner in node

---
+++ src/en/post/scan.data.ts
@@ -0,0 +1,21 @@
+import {createContentLoader} from "vitepress";
+
+export default {
+    async load() {
+        const all = await createContentLoader('src/en/post/*.md').load()
+        const docs = all
+            .filter((item) => {
+                const layout = item.frontmatter.layout
+                return layout === 'doc' || !layout
+            })
+            .map((item) => {
+                return {
+                    name: item.frontmatter.name,
+                    link: item.url,
+                    brief: item.frontmatter.brief,
+                }
+            })
+
+        return {docs}
+    }
+}
