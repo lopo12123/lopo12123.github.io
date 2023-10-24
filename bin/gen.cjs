103e9936abddd11db4d095ac464960232c189d02
lopo <lopo@zju.edu.cn>
2023-10-24 22:05:34 +0800

    feat: generator

--- bin/gen.cjs
+++ bin/gen.cjs
@@ -1,20 +1,12 @@
 /// generate a template post in specified directory
 /// Usage: gen <category/filename>
 
-const { resolve } = require('node:path')
-const { existsSync, mkdirSync, writeFileSync } = require('node:fs')
+const {resolve} = require('node:path')
+const {writeFileSync, readFileSync} = require('node:fs')
 
-const generateNewMd = (root, filename) => {
+const generateNewMd = (root, filename, content) => {
     try {
-        const isNew = !existsSync(root)
-
-        if (isNew) {
-            mkdirSync(root)
-            writeFileSync(resolve(root, 'index.md'), configDataTs)
-            writeFileSync(resolve(root, 'config.data.ts'), indexMD)
-        }
-
-        writeFileSync(resolve(root, `${filename}.md`), `---\nlayout: doc\n---\n\n# ${filename}`, 'utf-8')
+        writeFileSync(resolve(root, `${filename}.md`), content, 'utf-8')
 
         console.log(`Successfully generated ${root}/${filename}.md`)
     } catch (err) {
@@ -25,15 +17,14 @@ const generateNewMd = (root, filename) => {
 // ==================== separate ====================
 
 // yarn gen [post/project] <filename>
-const [ category, filename ] = process.argv.slice(2)
+const [category, filename] = process.argv.slice(2)
 
 if (!/^post|project$/.test(category)) {
-    console.error('Invalid category, should be <post> or <project>')
-}
-else if (/\w+/.test(filename)) {
-    console.error('Invalid filename, should be a word')
-}
-else {
-    generateNewMd(resolve(__dirname, '../src/en', category), filename)
-// generateNewMd(resolve(__dirname, '../src/zh', category), filename)
+    console.error(`Invalid category, should be <post> or <project> | got ${category}`)
+} else if (!/\w+/.test(filename)) {
+    console.error(`Invalid filename, should be a word | got ${filename}`)
+} else {
+    const template = readFileSync(resolve(__dirname, '../template.md'), 'utf-8')
+    generateNewMd(resolve(__dirname, '../src/en', category), filename, template)
+// generateNewMd(resolve(__dirname, '../src/zh', category), filename, template)
 }
