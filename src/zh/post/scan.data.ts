030b861cd8c6cdf81b1a9d31c8ca7033ab2b8b9f
lopo <lopo@zju.edu.cn>
2023-10-26 21:59:15 +0800

    feat: outline of zh/post

---
+++ src/zh/post/scan.data.ts
@@ -0,0 +1,21 @@
+import {createContentLoader} from "vitepress";
+
+export default {
+    async load() {
+        const all = await createContentLoader('src/zh/post/*.md').load()
+        const docs = all
+            .filter((item) => {
+                const layout = item.frontmatter.layout
+                return layout === 'doc' || !layout
+            })
+            .map((item) => {
+                return {
+                    topic: item.frontmatter.topic,
+                    link: item.url,
+                    brief: item.frontmatter.brief,
+                }
+            })
+
+        return {docs}
+    }
+}
