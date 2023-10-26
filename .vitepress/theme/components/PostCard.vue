a867a270ba963e4493280f562411ca5bc0f547fe
lopo <lopo@zju.edu.cn>
2023-10-26 22:12:35 +0800

    feat: i18n support

--- .vitepress/theme/components/PostCard.vue
+++ .vitepress/theme/components/PostCard.vue
@@ -1,67 +1,70 @@
 <script setup lang="ts">
-import {useRouter} from "vitepress";
+import { useData, useRouter } from "vitepress";
 
 defineProps<{
-  link: string
-  title?: string
-  brief?: string
+    link: string
+    title?: string
+    brief?: string
 }>()
 
 const router = useRouter()
+const { localeIndex } = useData()
 </script>
 
 <template>
-  <div class="post-card">
-    <article class="box">
-      <h2 class="title" v-html="title"></h2>
-      <p v-if="brief" class="brief" v-html="brief"></p>
-      <p class="link" @click="router.go(link)">see detail →</p>
-    </article>
-  </div>
+    <div class="post-card">
+        <article class="box">
+            <h2 class="title" v-html="title"></h2>
+            <p v-if="brief" class="brief" v-html="brief"></p>
+            <p class="link" @click="router.go(link)">
+                {{ { 'en': 'see detail', 'zh': '查看详情' }[localeIndex]}} →
+            </p>
+        </article>
+    </div>
 </template>
 
 <style scoped>
 .post-card {
-  display: block;
-  border: 1px solid var(--vp-c-bg-soft);
-  border-radius: 12px;
-  height: 100%;
-  background-color: var(--vp-c-bg-soft);
-  transition: border-color 0.25s, background-color 0.25s;
+    display: block;
+    border: 1px solid var(--vp-c-bg-soft);
+    border-radius: 12px;
+    height: 100%;
+    background-color: var(--vp-c-bg-soft);
+    transition: border-color 0.25s, background-color 0.25s;
 }
 
 .box {
-  flex-direction: column;
-  padding: 24px;
-  height: 100%;
+    flex-direction: column;
+    padding: 24px;
+    height: 100%;
 }
 
 .title {
-  line-height: 24px;
-  font-size: 16px;
-  font-weight: 600;
+    line-height: 24px;
+    font-size: 16px;
+    font-weight: 600;
 }
 
 .brief {
-  flex-grow: 1;
-  padding-top: 8px;
-  line-height: 24px;
-  font-size: 14px;
-  font-weight: 500;
-  color: var(--vp-c-text-2);
+    flex-grow: 1;
+    padding-top: 8px;
+    line-height: 24px;
+    font-size: 14px;
+    font-weight: 500;
+    color: var(--vp-c-text-2);
 }
 
 .link {
-  padding-top: 8px;
-  color: var(--vp-c-brand-1);
-  font-size: 14px;
-  font-weight: 500;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
+    padding-top: 8px;
+    color: var(--vp-c-brand-1);
+    font-size: 14px;
+    font-weight: 500;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
 
-  &:hover {
-    text-decoration: underline;
-  }
+    &:hover {
+        text-decoration: underline;
+    }
 }
 </style>
