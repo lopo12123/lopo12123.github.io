5f57cb1fad4bab32101d4dfb7e44aa9db1b5eb16
lopo <lopo@zju.edu.cn>
2023-11-01 00:08:44 +0800

    feat: improve command's compatibility

--- bin/gen.cjs
+++ bin/gen.cjs
@@ -1,12 +1,12 @@
 /// generate a template post in specified directory
 /// Usage: gen <category/filename>
 
-const {resolve} = require('node:path')
-const {writeFileSync, readFileSync} = require('node:fs')
+const { resolve } = require('node:path')
+const { writeFileSync, readFileSync } = require('node:fs')
 
 const generateNewMd = (root, filename, content) => {
     try {
-        writeFileSync(resolve(root, `${filename}.md`), content, 'utf-8')
+        writeFileSync(resolve(root, `${filename}.draft.md`), content, 'utf-8')
 
         console.log(`Successfully generated ${root}/${filename}.md`)
     } catch (err) {
@@ -16,14 +16,24 @@ const generateNewMd = (root, filename, content) => {
 
 // ==================== separate ====================
 
+// both of the following commands are valid:
 // yarn gen [post/project] <filename>
-const [category, filename] = process.argv.slice(2)
+// yarn gen [post/project]/<filename>
+let [ category, filename ] = process.argv.slice(2)
+if (!filename && category.includes('/')) {
+    [ category, filename ] = category.split('/')
+}
+
+console.log('- Category:', category)
+console.log('- FileName:', filename)
 
 if (!/^post|project$/.test(category)) {
     console.error(`Invalid category, should be <post> or <project> | got ${category}`)
-} else if (!/\w+/.test(filename)) {
+}
+else if (!/\w+/.test(filename)) {
     console.error(`Invalid filename, should be a word | got ${filename}`)
-} else {
+}
+else {
     const template = readFileSync(resolve(__dirname, '../template.md'), 'utf-8')
     generateNewMd(resolve(__dirname, '../src/en', category), filename, template)
 // generateNewMd(resolve(__dirname, '../src/zh', category), filename, template)
