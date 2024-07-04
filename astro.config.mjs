5e08f06dfd864adfdee325d3ea70ec178ba36249
lopo <lopo@zju.edu.cn>
2024-07-04 22:53:52 +0800

    chore: apply default

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://lopo12123.github.io',
-    outDir: './docs',
     integrations: [ mdx(), sitemap() ],
 });
