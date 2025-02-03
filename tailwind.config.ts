6dfdc2abfed135de8b75b84b37b9af530fad8a11
lopo <lopo@zju.edu.cn>
2025-02-03 13:09:55 +0800

    feat: font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,25 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
-      },
+    content: [ "./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}" ],
+    theme: {
+        extend: {
+            fontFamily: {
+                "atkinson": "Atkinson",
+                "poets": "Poets Electra",
+                "jb-mono": "JetBrains Mono",
+                sans: [
+                    "Inter",
+                    "ui-sans-serif",
+                    "system-ui",
+                    "sans-serif",
+                    "Apple Color Emoji",
+                    "Segoe UI Emoji",
+                    "Segoe UI Symbol",
+                    "Noto Color Emoji",
+                ],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
