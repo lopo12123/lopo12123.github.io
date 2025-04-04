71878bc514a319ace57c44f2147355d05c0496f8
lopo <lopo@zju.edu.cn>
2025-04-05 00:40:28 +0800

    feat: custom style & dark mode

--- app/root.tsx
+++ app/root.tsx
@@ -5,12 +5,46 @@ import {
     Scripts,
     ScrollRestoration,
 } from "@remix-run/react";
-import type { ReactNode } from "react";
 
 import "./styles/font.css";
 import "./styles/global.css";
+import "./styles/custom.css";
 
-export function Layout({ children }: { children: ReactNode }) {
+const Header = () => {
+    const toggleTheme = () => {
+        const html = document.documentElement
+        if (html.className == 'dark') {
+            html.className = 'light'
+        } else if (html.className == 'light') {
+            html.className = 'dark'
+        } else {
+            // exception!
+            html.className = 'light'
+        }
+    }
+
+    return (
+        <header>
+            <div className={ 'content-body mx-auto flex items-center justify-between' }>
+                <a href="/">
+                    <img className={ 'logo logo-black dark:hidden' } height={ 48 }
+                         src="/lopo-black.svg" alt="logo"/>
+                    <img className={ 'logo logo-white hidden dark:inline-block' } height={ 48 }
+                         src="/lopo-white.svg" alt="logo"/>
+                </a>
+
+                <nav>
+                    {/* TODO: nav */ }
+                </nav>
+
+                {/* TODO: switch button */ }
+                <button onClick={ toggleTheme }>aaa</button>
+            </div>
+        </header>
+    )
+}
+
+export default function App() {
     return (
         <html lang="zh" className="light">
             <head>
@@ -24,14 +58,12 @@ export function Layout({ children }: { children: ReactNode }) {
                 <Links/>
             </head>
             <body>
-                { children }
+                <Header/>
+                <Outlet/>
+
                 <ScrollRestoration/>
                 <Scripts/>
             </body>
         </html>
     );
-}
-
-export default function App() {
-    return <Outlet/>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        incognito: 'incognito'
-      },
+    darkMode: 'class',
+    content: [ "./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}" ],
+    theme: {
+        extend: {
+            fontFamily: {
+                incognito: 'incognito'
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
