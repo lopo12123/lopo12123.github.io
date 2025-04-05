528fe6379038da18d51df721e143d60d5da7f85f
lopo <lopo@zju.edu.cn>
2025-04-06 00:14:13 +0800

    feat: about page

---
+++ app/routes/about.tsx
@@ -0,0 +1,18 @@
+const sections = [
+    'Little fly,\nThy summer’s play\nMy thoughtless hand\nHas brushed away.',
+    'Am not I\nA fly like thee?\nOr art not thou\nA man like me?',
+    'For I dance\nAnd drink and sing,\nTill some blind hand\nShall brush my wing.\n\n',
+    'If thought is life\nAnd strength and breath,\nAnd the want\nOf thought is death,',
+    'Then am I\nA happy fly,\nIf I live,\nOr if I die.',
+]
+
+export default function AboutPage() {
+    return (
+        <main className={ 'content-body font-poets text-center whitespace-pre-wrap' }>
+            <h1 className={ 'font-poets italic' }>The Fly</h1>
+            {
+                sections.map((section, index) => <p key={ index }>{ section }</p>)
+            }
+        </main>
+    )
+}
