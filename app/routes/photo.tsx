ad727d0881f175f285efd69296c66c2461bc8997
lopo <lopo@zju.edu.cn>
2025-04-08 00:20:07 +0800

    feat: photo page

---
+++ app/routes/photo.tsx
@@ -0,0 +1,9 @@
+export default function PhotoPage() {
+    return (
+        <main className={ 'content-body' }>
+            <h1>Photos ...</h1>
+
+            <p>👋 nothing here ...</p>
+        </main>
+    )
+}
