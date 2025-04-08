ebe31539329025ca83a6fe26f249eaaaf7deaf3b
lopo <lopo@zju.edu.cn>
2025-04-08 23:52:44 +0800

    refactor: archive script

--- scripts/archive.cjs
+++ scripts/archive.cjs
@@ -1,10 +1,6 @@
 const {execSync} = require("child_process")
-const {readdirSync, readFileSync, writeFileSync} = require('fs')
-const {join, basename} = require('path')
-
-const canonical = (filename) => {
-    return basename(filename, '.md').replace(/_/g, '-').toLowerCase()
-}
+const {readdirSync, readFileSync, writeFileSync, cpSync} = require('fs')
+const {join} = require('path')
 
 /**
  * Split markdown content into frontmatter & content body
@@ -24,14 +20,18 @@ const split = (markdown) => {
 
 const archive = (sub) => {
     const manifest = []
-    for (const filename of readdirSync(join(SOURCE_DIR, sub))) {
-        const raw = readFileSync(join(SOURCE_DIR, sub, filename), 'utf-8')
+
+    for (const id of readdirSync(join(SOURCE_DIR, sub))) {
+        cpSync(`${join(SOURCE_DIR, sub, id)}/`, `${join(MEDIA_DIR, sub, id)}/`, {
+            recursive: true, filter: (source, _) => !source.endsWith(".md"),
+        })
+
+        const raw = readFileSync(join(SOURCE_DIR, sub, id, '.md'), 'utf-8')
         const [meta, content] = split(raw.trim())
-        const fileId = canonical(filename)
-        meta['id'] = fileId
+        meta['id'] = id
         manifest.push(meta)
-        writeFileSync(join(ARCHIVE_DIR, sub, `${fileId}.content`), content, 'utf-8')
-        writeFileSync(join(ARCHIVE_DIR, sub, `${fileId}.metadata`), JSON.stringify(meta), 'utf-8')
+        writeFileSync(join(ARCHIVE_DIR, sub, `${id}.content`), content, 'utf-8')
+        writeFileSync(join(ARCHIVE_DIR, sub, `${id}.metadata`), JSON.stringify(meta), 'utf-8')
     }
     writeFileSync(join(ARCHIVE_DIR, sub, 'manifest.json'), JSON.stringify(manifest))
     console.log(`> "${sub}" archived`)
@@ -39,7 +39,8 @@ const archive = (sub) => {
 
 const ROOT_DIR = execSync('git rev-parse --show-toplevel').toString().trim()
 const SOURCE_DIR = join(ROOT_DIR, 'source')
+const MEDIA_DIR = join(ROOT_DIR, 'public', 'media')
 const ARCHIVE_DIR = join(ROOT_DIR, 'public', 'archive')
 
 archive('essay')
-archive('project')
+// archive('project')
