b6c0b0a4247e7512586a242140f08319e9c396c7
lopo <lopo@zju.edu.cn>
2025-04-08 23:49:10 +0800

    feat: type declare

---
+++ app/types.ts
@@ -0,0 +1,14 @@
+export type EssayMeta = {
+    id: string
+    title: string
+    poster?: string
+    datetime: string
+    tag: string[]
+}
+
+export type ProjectMeta = {
+    id: string
+    name: string
+    description: string
+    logo?: string
+}
