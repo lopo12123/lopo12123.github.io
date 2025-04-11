99f23938026935494906544d1d3cbee8b0fd72bf
lopo <lopo@zju.edu.cn>
2025-04-12 01:11:08 +0800

    feat: add loader

--- app/routes/essay.$essayId.tsx
+++ app/routes/essay.$essayId.tsx
@@ -1,10 +1,19 @@
 import { ClientLoaderFunctionArgs, useLoaderData } from "@remix-run/react";
 import { EssayMeta } from "~/types";
 import { parseMarkdown } from "~/utils/markdown";
+import { resources } from "~/utils/resource";
 
 type EssayLoaderData = {
     metadata: EssayMeta | null
     content: string | null
+}
+
+export const loader = ({ params }: ClientLoaderFunctionArgs) => {
+    const essayId = params['essayId']!
+    return {
+        metadata: resources.get('essay', essayId, 'metadata'),
+        content: parseMarkdown('essay', essayId, resources.get('essay', essayId, 'content')),
+    } satisfies EssayLoaderData
 }
 
 export const clientLoader = async ({ params }: ClientLoaderFunctionArgs) => {
@@ -21,7 +30,7 @@ export const clientLoader = async ({ params }: ClientLoaderFunctionArgs) => {
 }
 
 export default function EssayPage() {
-    const { metadata, content } = useLoaderData<EssayLoaderData>()
+    const { content } = useLoaderData<EssayLoaderData>()
 
     return (
         <main className={ 'content-body' }>
--- app/routes/essay._index.tsx
+++ app/routes/essay._index.tsx
@@ -1,9 +1,33 @@
+import { useLoaderData } from "@remix-run/react";
+import { EssayMeta } from "~/types";
+import { resources } from "~/utils/resource";
+
+export const loader = () => resources.get('essay')
+
+export const clientLoader = async () => {
+    return fetch('/archive/essay/manifest.json').then(r => r.json()) as unknown as EssayMeta[]
+}
+
 export default function EssayGalleryPage() {
+    const manifest = useLoaderData<EssayMeta[]>()
+
     return (
         <main className={ 'content-body' }>
             <h1>Essays ...</h1>
 
             <p>✍️ producing ...</p>
+
+            <ul>
+                {
+                    manifest.map(({ id, title }) => {
+                        return (
+                            <li key={ id }>
+                                <a href={ `/essay/${ id }` }>{ title }</a>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </main>
     )
 }
