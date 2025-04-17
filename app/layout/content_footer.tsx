25e86a3cb3d669a45c47b9370b39df40f817ef30
lopo <lopo@zju.edu.cn>
2025-04-17 22:09:03 +0800

    chore: hide unsafe item

--- app/layout/content_footer.tsx
+++ app/layout/content_footer.tsx
@@ -6,10 +6,10 @@ const ContentFooter = () => {
         <footer>
             <hr/>
             <ul>
-                <li>
-                    <IconArrowRight className={ 'text-[20px] mr-1' }/>
-                    <a href="javascript:window.scrollTo({top: 0, behavior: 'smooth'});">Back to top</a>
-                </li>
+                {/*<li>*/ }
+                {/*    <IconArrowRight className={ 'text-[20px] mr-1' }/>*/ }
+                {/*    <a href="javascript:window.scrollTo({top: 0, behavior: 'smooth'});">Back to top</a>*/ }
+                {/*</li>*/ }
                 <li>
                     <IconArrowRight className={ 'text-[20px] mr-1' }/>
                     <Link to={ '/essay' }>All Essay</Link>
