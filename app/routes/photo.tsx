748ae0ee92f510343cab1fe1ce6ccedb4f264121
lopo <lopo@zju.edu.cn>
2025-05-31 23:00:16 +0800

    chore: clean up

--- app/routes/photo.tsx
+++
@@ -1,9 +0,0 @@
-export default function PhotoPage() {
-    return (
-        <main className={ 'content-body' }>
-            <h1>Photos ...</h1>
-
-            <p>🤷 nothing here ...</p>
-        </main>
-    )
-}
