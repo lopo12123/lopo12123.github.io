e42aa23b5fd9508c424bf6bf318267af4c5e3e43
lopo <lopo@zju.edu.cn>
2025-06-01 00:05:18 +0800

    chore: nav config

--- src/config/nav_item.ts
+++ src/config/nav_item.ts
@@ -6,7 +6,7 @@ type NavItem = {
 const navItems: NavItem[] = [
     { title: 'Essay', path: '/essay' },
     // { title: 'Project', path: '/project' },
-    { title: 'Photo', path: '/photo' },
+    // { title: 'Photo', path: '/photo' },
     // { title: 'Annual', path: '/annual' },
     // { title: 'The Fly', path: '/the-fly' },
     // { title: 'About', path: '/about' },
