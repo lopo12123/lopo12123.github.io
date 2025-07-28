de5fde6df8095a48a070fd31f0501c9fe599b3df
lopo <lopo@zju.edu.cn>
2025-07-28 23:48:32 +0800

    feat: home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,19 @@
-import type { Route } from "./+types/home";
-
 export default function HomePage() {
     return (
-        <div className={'h-[1000px]'}>
-            Home page
-        </div>
+        <>
+            <section className={ 'max-w-[768px] mx-auto pt-8' }>
+                <h1 className={ 'my-2 text-3xl font-medium' }>
+                    <span>Hi there!</span>
+                    <span className={ 'wave-hands ml-2' }>👋</span>
+                </h1>
+                <p className={ 'text-[#282728] font-light' }>
+                    It's lopo, a website and app developer who uses js and dart and is interested in rust.
+                </p>
+            </section>
+
+            <section>
+                {/*  TODO: recent posts  */ }
+            </section>
+        </>
     )
 }
